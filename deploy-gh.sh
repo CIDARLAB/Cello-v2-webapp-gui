19391ad924b6f65c91098fe4d8a088274aa51678
Tim Jones <tjones01@gmail.com>
2019-03-13 16:37:01 -0400

    Remove angular.json, platforms/, plugins/ in deploy.sh.

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -9,18 +9,10 @@ rm README.md
 rm -rf src
 rm node_modules
 cp -r www/* .
-rm -rf www
-rm ionic.config.json
-rm package.json
-rm package-lock.json
-rm tsconfig.json
-rm tslint.json
-rm -rf node_modules
-rm -rf resources
-rm -rf e2e
-rm config.xml
+rm .gitignore config.xml angular.json ionic.config.json package.json package-lock.json tsconfig.json tslint.json
+rm -rf www node_modules resources e2e platforms plugins
 git add .
 git reset deploy-gh.sh
-git commit -m "Publishing to gh-pages"
+git commit -m "Publishing to gh-pages."
 git push -u origin gh-pages
 git checkout develop
