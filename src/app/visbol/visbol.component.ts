43e4b9ebfd668f6981aedb710a5ef69e315ca28d
Tim Jones <tjones01@gmail.com>
2019-03-14 09:20:22 -0400

    ViSbol component.

---
+++ src/app/visbol/visbol.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visbol',
+  templateUrl: './visbol.component.html',
+  styleUrls: ['./visbol.component.scss'],
+})
+export class VisbolComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
