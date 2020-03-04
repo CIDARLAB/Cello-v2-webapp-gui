0671ee68ba5df2aa51121a4db41f0bdf22037fd1
Timothy Jones <jonests@bu.edu>
2020-03-04 15:11:27 -0500

    Deploy to `production` branch.

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 git checkout develop
-git branch -D gh-pages
-git push origin --delete gh-pages
-git checkout -b gh-pages
+git branch -D production
+git push origin --delete production
+git checkout -b production
 npm install
-ionic cordova build browser --prod
+ionic build --prod
 rm -rf $(ls | grep -v www)
 cp -r www/* .
 rmdir www
 rm .travis.yml
 git add .
-git commit -S -m "Publishing to gh-pages."
-git push -u origin gh-pages
+git commit -S -m "Publishing to `production` branch."
+git push -u origin production
 git checkout develop
