9bc1fd231c77fb7d71e5de827dd6be904bfd0360
Timothy Jones <jonests@bu.edu>
2020-06-01 13:09:48 -0400

    Add @angular/material.

--- src/app/results/results.module.ts
+++ src/app/results/results.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { IonicModule } from '@ionic/angular';
 import { ResultsListComponent } from './results-list/results-list.component';
 import { ResultsRoutingModule } from './results-routing.module';
@@ -7,6 +8,6 @@ import { ResultsComponent } from './results/results.component';
 
 @NgModule({
   declarations: [ResultsListComponent, ResultsComponent],
-  imports: [CommonModule, IonicModule, ResultsRoutingModule],
+  imports: [CommonModule, IonicModule, ResultsRoutingModule, MatExpansionModule],
 })
 export class ResultsModule {}
