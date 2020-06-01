6513f25499fdc86a1d1aeb1f109e89c599b3e3c5
Timothy Jones <jonests@bu.edu>
2020-06-01 19:01:38 -0400

    Add iterable pipe.

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { LoaderComponent } from './loader/loader.component';
+import { IterablePipe } from './iterable/iterable.pipe';
 
 @NgModule({
   imports: [IonicModule, CommonModule],
-  declarations: [LoaderComponent],
+  declarations: [LoaderComponent, IterablePipe],
   exports: [LoaderComponent],
 })
 export class SharedModule {}
