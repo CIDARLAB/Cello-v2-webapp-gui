5759ef80109521b9f17aeb4e7749d4d219b66b4c
Timothy Jones <jonests@bu.edu>
2020-06-01 20:35:00 -0400

    Remove Iterable pipe.

--- src/app/@shared/iterable/iterable.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IterablePipe } from './iterable.pipe';
-
-describe('IterablePipe', () => {
-  it('create an instance', () => {
-    const pipe = new IterablePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -2,11 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { LoaderComponent } from './loader/loader.component';
-import { IterablePipe } from './iterable/iterable.pipe';
 
 @NgModule({
   imports: [IonicModule, CommonModule],
-  declarations: [LoaderComponent, IterablePipe],
+  declarations: [LoaderComponent],
   exports: [LoaderComponent],
 })
 export class SharedModule {}
