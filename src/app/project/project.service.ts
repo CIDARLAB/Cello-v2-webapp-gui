304ee695b5b14fe8fbdd1f79e4e52f24d9763658
W.R. Jackson <jackson@justjackson.dev>
2021-04-07 16:10:01 -0400

    Stop user from moving forward when input sequence is incomplete

--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -49,30 +49,74 @@ export class ProjectService {
     return await toast.present();
   }
 
+  async validateProject() {
+    const { name, description, created, verilog, settings, library } = this.project;
+    const { input_constraints, output_constraints } = this.project.constraints;
+    let result = true;
+    if (verilog === undefined) {
+      Promise.resolve().then(() => {
+        this.toast('Input Verilog File not Found. ' + 'Please check Input Verilog File and resubmit.');
+      });
+      result = result && false;
+    }
+    if (!settings) {
+      Promise.resolve().then(() => {
+        this.toast('Input Settings File not Found. ' + 'Please check Input Settings File and resubmit.');
+      });
+      result = result && false;
+    }
+    if (!library) {
+      Promise.resolve().then(() => {
+        this.toast('Input Library File not Found. ' + 'Please check Input Library File and resubmit.');
+      });
+      result = result && false;
+    }
+    if (!input_constraints) {
+      Promise.resolve().then(() => {
+        this.toast('Input Constraints not Found. ' + 'Please validated UCF File and resubmit.');
+      });
+      result = result && false;
+    }
+    if (!output_constraints) {
+      Promise.resolve().then(() => {
+        this.toast('Output Constraints not Found. ' + 'Please validate UCF File and resubmit.');
+      });
+      result = result && false;
+    }
+    return result;
+  }
+
   async submit() {
+    console.log('Within the Submission.');
     const name = this.project.name;
     this.isLoading = true;
     const loadingOverlay = await this.loadingController.create({});
-    return Promise.resolve()
-      .then(() => {
-        // let body = this.project.getSpecification();
-        this.toast('Submitting project. Results will appear after successful execution.');
-        const loading$ = from(loadingOverlay.present());
-        return this.apiService.createProject(this.project).toPromise();
-      })
-      .then(() => {
-        if (this.project.name == name) {
-          this.apiService.getProjectResults(name).subscribe((result) => {
-            this.project.results = result.sort((a, b) => (a.name > b.name ? 1 : -1));
-          });
-          this.isLoading = false;
-          loadingOverlay.dismiss();
-          this.toast('Project ' + name + ' finished successfully.', 'success');
-        }
-      })
-      .catch((error) => {
-        this.toastController.dismiss();
-        this.alert(error.error.message);
-      });
+    const validated = Promise.resolve().then(() => {
+      console.log('Validation Conditional {Chain 1}.');
+      this.validateProject();
+    });
+    if (validated) {
+      return Promise.resolve()
+        .then(() => {
+          // let body = this.project.getSpecification();
+          this.toast('Submitting project. Results will appear after successful execution.');
+          const loading$ = from(loadingOverlay.present());
+          return this.apiService.createProject(this.project).toPromise();
+        })
+        .then(() => {
+          if (this.project.name === name) {
+            this.apiService.getProjectResults(name).subscribe((result) => {
+              this.project.results = result.sort((a, b) => (a.name > b.name ? 1 : -1));
+            });
+            this.isLoading = false;
+            loadingOverlay.dismiss();
+            this.toast('Project ' + name + ' finished successfully.', 'success');
+          }
+        })
+        .catch((error) => {
+          this.toastController.dismiss();
+          this.alert(error.error.message);
+        });
+    }
   }
 }
